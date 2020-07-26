f6c633963973333e789ebfcedb8c37d765af5c16
Krzysztof <kubiak.krzysztof@outlook.com>
2020-07-26 12:29:44 +0200

    Solve probelms with return data from promise

--- src/svg.js
+++ src/svg.js
@@ -759,22 +759,26 @@ Celestial.exportSVG = function(fname) {
   }
 
   if (!fname) {
-    console.log('LACK OF FNAME');
-    var svgExp = d3.select("#d3-celestial-svg svg")
-        .attr("title", "D3-Celestial")
-        .attr("version", 1.1)
-        .attr("encoding", "UTF-8")
-        .attr("xmlns", "http://www.w3.org/2000/svg")
-        .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
-        .attr("xmlns:sodipodi", "http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd")
-        .attr("xmlns:inkscape", "http://www.inkscape.org/namespaces/inkscape")
-        .attr("viewBox", " 0 0 " + (m.width) + " " + (m.height));
+    return new Promise(function(resolve, reject) {
+      q.await(function(error) {
+        if (error) throw error;
+        var svgExp = d3.select("#d3-celestial-svg svg")
+            .attr("title", "D3-Celestial")
+            .attr("version", 1.1)
+            .attr("encoding", "UTF-8")
+            .attr("xmlns", "http://www.w3.org/2000/svg")
+            .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
+            .attr("xmlns:sodipodi", "http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd")
+            .attr("xmlns:inkscape", "http://www.inkscape.org/namespaces/inkscape")
+            .attr("viewBox", " 0 0 " + (m.width) + " " + (m.height));
 
-    defs.append("style")
-        .attr("type", "text\/css")
-        .text(createStyles());
+        defs.append("style")
+            .attr("type", "text\/css")
+            .text(createStyles());
 
-    return svgExp.node().outerHTML;
+        resolve(svgExp.node().outerHTML);
+      });
+    });
   }
 
   q.await(function(error) {
