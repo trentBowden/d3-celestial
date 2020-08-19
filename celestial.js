e27740063d09e7afdad7f5d2742a8e15d85e6192
Krzysztof <kubiak.krzysztof@outlook.com>
2020-08-19 20:26:08 +0200

    Resign with dynamic size SVG

--- celestial.js
+++ celestial.js
@@ -4585,12 +4585,6 @@ Celestial.exportSVG = function(fname) {
       svg = d3.select("#d3-celestial-svg").append("svg"), //.attr("style", "display: none"),
       m = Celestial.metrics(),
       cfg = settings.set();
-
-  // if (cfg.svgSize) {
-    m.width = 618;
-    m.height = 618;
-  // }
-
   var path = cfg.datapath,
       proj = projections[cfg.projection],
       rotation = getAngles(cfg.center),
